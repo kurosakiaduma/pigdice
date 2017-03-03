c430405ce298bdd7faa2819c7caca3894a5181da
Tevin Joel Aduma <tevin74@live.com>
2017-03-03 17:32:50 +0300

    create a custom fucntion to display the result using the 2D-image file for the dice

--- js/script1.js
+++ js/script1.js
@@ -14,5 +14,10 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         var dice1 = Math.floor(Math.random() * 6) + 1;
         var dice2 = Math.floor(Math.random() * 6) + 1;
 
+        //2. Display the result
+                document.getElementById('dice-1').style.display = 'block';
+                document.getElementById('dice-2').style.display = 'block';
+                document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+                document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
 });
