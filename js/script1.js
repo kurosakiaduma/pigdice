13f1cd1b491532316c0299ea82021f77f836e57a
Tevin Joel Aduma <tevin74@live.com>
2017-03-03 17:29:57 +0300

    create a custom fucntion to generate random numbers when the user clicks the roll-dice button

--- js/script1.js
+++ js/script1.js
@@ -10,6 +10,9 @@ var lastDice;
 /*Backend logic*/
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
+        // 1. Random number
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
 
 
 });
