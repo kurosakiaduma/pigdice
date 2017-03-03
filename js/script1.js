f68ed9bff5ba39d2b720bbc04d174ee8368d1036
Tevin Joel Aduma <tevin74@live.com>
2017-03-03 17:47:18 +0300

    create a constructor for nextPlayer when previous game ends for the start of a new game

--- js/script1.js
+++ js/script1.js
@@ -85,5 +85,24 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
               });
 
 
+              function nextPlayer() {
+                  //Next player
+                  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+                  roundScore = 0;
+
+                  document.getElementById('current-0').textContent = '0';
+                  document.getElementById('current-1').textContent = '0';
+
+                  document.querySelector('.player-0-panel').classList.toggle('active');
+                  document.querySelector('.player-1-panel').classList.toggle('active');
+
+                  //document.querySelector('.player-0-panel').classList.remove('active');
+                  //document.querySelector('.player-1-panel').classList.add('active');
+
+                  document.getElementById('dice-1').style.display = 'none';
+                  document.getElementById('dice-2').style.display = 'none';
+              }
+
+
 
 });
