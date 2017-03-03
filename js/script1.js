6a6db9e69a0d5615ddbde429f48bf488084362bc
Tevin Joel Aduma <tevin74@live.com>
2017-03-03 17:38:13 +0300

    add conditions that update the round score if the rolled number was NOT a 1

--- js/script1.js
+++ js/script1.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 var scores, roundScore, activePlayer, gamePlaying;
 
-init();
+
 
 var lastDice;
 
@@ -19,5 +19,34 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
                 document.getElementById('dice-2').style.display = 'block';
                 document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
                 document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
+                //3. Update the round score IF the rolled number was NOT a 1
+                      if (dice1 !== 1 && dice2 !== 1) {
+                          //Add score
+                          roundScore += dice1 + dice2;
+                          document.querySelector('#current-' + activePlayer).textContent = roundScore;
+                      } else {
+                          //Next player
+                          nextPlayer();
+                      }
+
+                      /*
+                      if (dice === 6 && lastDice === 6) {
+                          //Player looses score
+                          scores[activePlayer] = 0;
+                          document.querySelector('#score-' + activePlayer).textContent = '0';
+                          nextPlayer();
+                      } else if (dice !== 1) {
+                          //Add score
+                          roundScore += dice;
+                          document.querySelector('#current-' + activePlayer).textContent = roundScore;
+                      } else {
+                          //Next player
+                          nextPlayer();
+                      }
+                      lastDice = dice;
+                      */
+                  }
+              });
 
 });
